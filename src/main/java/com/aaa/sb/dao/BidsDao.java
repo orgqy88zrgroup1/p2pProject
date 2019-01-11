2a0c3838e89de0e8252dff97b6682338563146ed
zxl1314123 <652480469@qq.com>
2019-01-11 10:16:59 +0800

    张新磊333333333

--- src/main/java/com/aaa/sb/dao/BidsDao.java
+++ src/main/java/com/aaa/sb/dao/BidsDao.java
@@ -15,10 +15,10 @@ public interface BidsDao {
      */
     @Select("<script>select id,realname,userid,auditid,bidid,to_char(audittime,'yyyy-mm-dd HH24:mi:ss') as audittime,auditresult,auditremarks,bidamount,bidrepaymentmethod,bidrate,biddesc,bidDeadDay,bidState from (" +
             "select rownum rn ,a.id,a.userid,a.bidid,r.realname,a.auditid,a.audittime,a.auditresult,a.auditremarks,b.bidamount,b.bidrepaymentmethod,b.bidrate,b.biddesc,b.bidDeadDay,b.bidState from " +
-            "bid_audit a  join bid_info b on a.userid=b.userid  join realname_certification r on r.userid=a.userid where rownum &lt; #{end} " +
+            "bid_audit a  join bid_info b on a.userid=b.userid  join realname_certification r on r.userid=a.userid where 1=1 and  b.bidState in (0,1,8) and  rownum &lt; #{end} " +
             "<if test=\"realname!=null and realname!=''\">and r.realname like '%'||#{realname}||'%'</if>"+
             "<if test=\"auditresult!=null and auditresult!=''\">and a.auditresult=#{auditresult}</if>"+
-            ") c where c.rn &gt; #{start}</script>")
+            ") c where  c.rn &gt; #{start}  </script>")
     List<Map> getPageByParam(Map map);
 
     /**
@@ -26,9 +26,9 @@ public interface BidsDao {
      * @param map
      * @return
      */
-    @Select("<script>select count(*) from bid_audit <where>" +
-            "<if test=\"realname!=null and realname!=''\">and realname like '%'||#{realname}||'%'</if>"+
-            "<if test=\"auditresult!=null and auditresult!=''\">and auditresult=#{auditresult}</if>"+
+    @Select("<script>select count(*) from bid_audit a join bid_info b on a.userid=b.userid  join realname_certification r on r.userid=a.userid <where> 1=1 and  b.bidState in (0,1,8) " +
+            "<if test=\"realname!=null and realname!=''\">and r.realname like '%'||#{realname}||'%'</if>"+
+            "<if test=\"auditresult!=null and auditresult!=''\">and a.auditresult=#{auditresult}</if>"+
             "</where></script>")
     int getPageCount(Map map);
     /**
@@ -36,7 +36,7 @@ public interface BidsDao {
      * @param map
      * @return
      */
-    @Insert("insert into bid_audit values(seq_bidaudit_id.nextval,#{USERID},#{AUDITID},sysdate,#{AUDITRESULT},#{BIDID},#{AUDITREMARKS})")
+    @Insert("update bid_audit set audittime=sysdate,auditresult=#{AUDITRESULT},auditremarks=#{AUDITREMARKS} where userid=#{USERID}")
     int update(Map map);
 
     /**
